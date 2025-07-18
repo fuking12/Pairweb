138ffc88a9933222d9f7ccb9eabefd93e6909482
fuking12 <pqthumchinthaka105@gmail.com>
2025-07-18 17:02:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-__path = process.cwd();
+const fs = require("fs");
+const __path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let code = require("./pair");
 require("events").EventEmitter.defaultMaxListeners = 500;
+
+// Create session directory if it doesn't exist
+if (!fs.existsSync("./session")) {
+  fs.mkdirSync("./session");
+}
+
 app.use("/code", code);
 
 app.use("/", async (req, res, next) => {
@@ -14,7 +21,7 @@ app.use("/", async (req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
+  console.log(`⏩ Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
